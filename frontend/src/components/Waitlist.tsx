df8d9b30447d6d723078bc7eda2dfd6cb7d970fb
Hariharan <hariharanc5767@gmail.com>
2026-06-28 22:52:48 +0530

    Bug fix

--- frontend/src/components/Waitlist.tsx
+++ frontend/src/components/Waitlist.tsx
@@ -29,7 +29,7 @@ export default function ContactUs() {
     setIsLoading(true);
     setApiError('');
     try {
-      const res = await fetch('http://localhost:8000/api/contact/', {
+      const res = await fetch('/api/contact/', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
